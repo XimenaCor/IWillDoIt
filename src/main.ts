762fe5f78b7a2ca45d51ab6db3569c1b5eaae147
Ximena Cordero <cor.xime@gmail.com>
2026-01-18 17:39:18 +0100

    fix(fly): listen on 0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { LoggingInterceptor } from './common/logging.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new LoggingInterceptor());
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = Number(process.env.PORT) || 3000;
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`🚀 App listening on port ${port}`);
 }
+
 void bootstrap();
